2b5b96d9d4d7bc595de17ec5d739ba42197e70c7
César García <cesargarciacampana@gmail.com>
2022-11-06 16:15:47 +0100

    KickStart2013 - RoundA - PalindromeFreeStrings with tests

---
+++ KickStart2013/RoundA.Tests/PalindromeFreeStringTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundA.Tests
+{
+	internal class PalindromeFreeStringTests
+	{
+		static IOHelper[] testCases = new IOHelper[]
+		{
+			new IOHelper(
+ @"2
+9
+100???001
+5
+100??",
+@"Case #1: IMPOSSIBLE
+Case #2: POSSIBLE
+"),
+
+new IOHelper(
+ @"2
+9
+?
+5
+?1?",
+@"Case #1: POSSIBLE
+Case #2: POSSIBLE
+"),
+
+			new IOHelper(
+ @"4
+9
+111100101100101100101?
+9
+1111001011001?1100101?
+5
+111100101100101100101111?0000
+5
+1111?0000",
+@"Case #1: POSSIBLE
+Case #2: POSSIBLE
+Case #3: IMPOSSIBLE
+Case #4: IMPOSSIBLE
+"),
+
+new IOHelper(
+ @"6
+9
+1000?1110
+13
+100110?100110
+6
+?0101?
+5
+?????
+6
+01111?
+20
+1000111",
+@"Case #1: IMPOSSIBLE
+Case #2: IMPOSSIBLE
+Case #3: POSSIBLE
+Case #4: POSSIBLE
+Case #5: IMPOSSIBLE
+Case #6: IMPOSSIBLE
+"),
+
+new IOHelper(
+ @"5
+4
+????
+5
+?????
+7
+?11111?
+5
+1?0?1
+5
+10001",
+@"Case #1: POSSIBLE
+Case #2: POSSIBLE
+Case #3: IMPOSSIBLE
+Case #4: POSSIBLE
+Case #5: IMPOSSIBLE
+"),
+
+		};
+
+		[Test]
+		[TestCaseSource(nameof(testCases))]
+		public void PalindromeFreeStringTest(IOHelper testCase)
+		{
+			RoundA.Program.PalindromeFreeStrings(testCase);
+
+			Assert.IsTrue(testCase.ExpectedOutput == testCase.Output.ToString());
+		}
+
+		[TestCase("10010", ExpectedResult = true)]
+		[TestCase("00000", ExpectedResult = false)]
+		[TestCase("1001001", ExpectedResult = false)]
+		[TestCase("10011001", ExpectedResult = false)]
+		[TestCase("01110", ExpectedResult = false)]
+		[TestCase("0100011", ExpectedResult = false)]
+		public bool IsPalinfromeFreeTest(string text)
+		{
+			return Program.IsPalindromeFree(text.ToCharArray(), text.Length - 1);
+		}
+	}
+}
--- KickStart2013/RoundA/Program.cs
+++ KickStart2013/RoundA/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace RoundA
 {
@@ -8,8 +9,10 @@ namespace RoundA
 	{
 		static void Main(string[] args)
 		{
-			//SpeedTyping(new ConsoleIO());
-			ChallengeNine(new ConsoleIO());
+			var console = new ConsoleIO();
+			//SpeedTyping(console);
+			//ChallengeNine(console);
+			PalindromeFreeStrings(console);
 		}
 
 		/// <summary>
@@ -29,11 +32,11 @@ namespace RoundA
 				int nRemoved = 0;
 
 				int ip = 0;
-				for(int ii = 0; ii < desired.Length; ii++)
+				for (int ii = 0; ii < desired.Length; ii++)
 				{
 					int temp = ip;
 					ip = typed.IndexOf(desired[ii], ip);
-					
+
 					if (ip == -1)
 					{
 						possible = false;
@@ -49,7 +52,7 @@ namespace RoundA
 				if (possible)
 					console.print(i + 1, nRemoved);
 				else
-                    console.print(i + 1, "IMPOSSIBLE");
+					console.print(i + 1, "IMPOSSIBLE");
 			}
 		}
 
@@ -78,7 +81,7 @@ namespace RoundA
 				{
 					bool added = false;
 					int j = 0;
-					while(j < number.Length)
+					while (j < number.Length)
 					{
 						char c = number[j];
 						if (CharToInt(c) > needed)
@@ -102,38 +105,131 @@ namespace RoundA
 			}
 		}
 
-        public class ConsoleIO
-        {
-            public virtual string ReadLine()
-            {
-                return Console.ReadLine();
-            }
+		/// <summary>
+		/// https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb33e/00000000009e762e
+		/// </summary>
+		/// <param name="console"></param>
+		public static void PalindromeFreeStrings(ConsoleIO console)
+		{
+			int nTests = console.readInt();
 
-            public virtual void WriteLine(string text)
-            {
-                Console.WriteLine(text);
-            }
-            public int readInt()
-            {
-                return Convert.ToInt32(ReadLine());
-            }
+			for (int i = 0; i < nTests; i++)
+			{
+				int length = console.readInt();
+				bool possible = true;
+				char[] word = console.ReadLine().ToCharArray();
 
-            public void print(int nCase, string value)
-            {
-				if (nCase == 0)
-					throw new Exception("nCase can't be zero");
+				possible = CanBePalindromeFree(word, 0);
 
-                WriteLine($"Case #{nCase}: {value}");
-            }
+				string result = possible ? "POSSIBLE" : "IMPOSSIBLE";
+				console.print(i + 1, result);
+			}
+		}
 
-            public void print(int nCase, int value)
-            {
+		private static bool CanBePalindromeFree(char[] text, int index)
+		{
+			if (index >= text.Length)
+				return IsPalindromeFree(text, text.Length - 1);
+
+			if (text[index] == '?')
+			{
+				text[index] = '0';
+				if (CanBePalindromeFree(text, index + 1))
+					return true;
+
+				text[index] = '1';
+				if (CanBePalindromeFree(text, index + 1))
+					return true;
+
+				text[index] = '?';
+
+				return false;
+			}
+			else
+			{
+				if (!IsPalindromeFree(text, index))
+					return false;
+
+				return CanBePalindromeFree(text, index + 1);
+			}
+		}
+
+		public static bool IsPalindromeFree(char[] text, int endIndex)
+		{
+			int length = 5;
+
+			while (length <= 6)
+			{
+				if (!IsPalindromeFree(text, length, endIndex))
+					return false;
+
+				length++;
+			}
+
+			return true;
+		}
+
+		public static bool IsPalindromeFree(char[] text, int length, int endIndex)
+		{
+			int start = 0;
+			while (start <= endIndex + 1 - length)
+			{
+				int end = start + length - 1;
+				bool hasPal = true;
+				int i = start;
+				int j = end;
+				while (j > i)
+				{
+					if (text[i] != text[j])
+					{
+						hasPal = false;
+						break;
+					}
+					i++;
+					j--;
+				}
+				if (hasPal)
+					return false;
+
+				start++;
+
+			}
+
+			return true;
+		}
+
+		public class ConsoleIO
+		{
+			public virtual string ReadLine()
+			{
+				return Console.ReadLine();
+			}
+
+			public virtual void WriteLine(string text)
+			{
+				Console.WriteLine(text);
+			}
+			public int readInt()
+			{
+				return Convert.ToInt32(ReadLine());
+			}
+
+			public void print(int nCase, string value)
+			{
 				if (nCase == 0)
 					throw new Exception("nCase can't be zero");
 
 				WriteLine($"Case #{nCase}: {value}");
-            }
-        }
+			}
 
-    }
+			public void print(int nCase, int value)
+			{
+				if (nCase == 0)
+					throw new Exception("nCase can't be zero");
+
+				WriteLine($"Case #{nCase}: {value}");
+			}
+		}
+
+	}
 }
